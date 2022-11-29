623d7b1af2c0edff09127a9f8dd1e4ca271775b0
Mussa Muhindo Justin <107538656+muhissakings@users.noreply.github.com>
2022-11-30 00:14:05 +0100

    Delete factorise.py

--- factorise.py
+++
@@ -1,9 +0,0 @@
-tests=int(input("Enter a number: "))
-
-for test in range(tests):
-            n=int(input())
-            while n // 5 != 0:
-                        n = n//5
-                        zeros += n
-            print(zeros)
-                        
